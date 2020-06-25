7e41c4e2d05f7a3567093058676d742765c84d57
Yu Nishimura <yunishimura0716@gmail.com>
2020-06-25 10:57:09 -0700

    separte the databse setting into local and heroku

--- logo/settings.py
+++ logo/settings.py
@@ -88,24 +88,19 @@ WSGI_APPLICATION = 'logo.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 # for local environment
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 # for server environment
 if HEROKU_ENV:
-    DATABASE_URL = 'postgresql://<postgresql>'
-    DATABASE_NAME = 'postgresql'
+    DATABASES = {
+        'default': dj_database_url.config(default=config('DATABASE_URL'))
+    }
 else:
-    DATABASE_URL = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
-    DATABASE_NAME = os.path.join(BASE_DIR, 'db.sqlite3')
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
-DATABASES = {
-    'default': dj_database_url.config()
-}
-DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
 # DATABASES['default']['NAME'] = DATABASE_NAME
 
 
