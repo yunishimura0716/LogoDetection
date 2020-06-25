2cde09c9c79293689a6b930937837d17c769747e
Yu Nishimura <yunishimura0716@gmail.com>
2020-06-25 11:25:31 -0700

    bit change in db setting

--- logo/settings.py
+++ logo/settings.py
@@ -89,21 +89,19 @@ WSGI_APPLICATION = 'logo.wsgi.application'
 
 # for local environment
 # for server environment
-DATABASES = {}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 if HEROKU_ENV:
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
     # DATABASES = {
     #     'default': dj_database_url.config()
     # }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
-
+    
 # DATABASES['default']['NAME'] = DATABASE_NAME
 
 
